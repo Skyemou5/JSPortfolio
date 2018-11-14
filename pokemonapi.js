48e14d211ee5000383d7f1d97149e5338bf14995
Ben Shurts <benjaminshurts@gmail.com>
2018-11-13 23:20:43 -0700

    card flip practice

--- pokemonapi.js
+++ pokemonapi.js
@@ -9,16 +9,8 @@ cardDOM.innerText = "This is a new div"
 //target where to put the DOM object
 document.getElementById('practice').appendChild( cardDOM )
 
-//card flip----------------
-
-// var card = document.querySelector('.cardContainer');
-// card.addEventListener( 'click', function() {
-//     // console.log("click")
-//     card.classList.toggle('is-flipped');
-// });
 
 
-//practice----------------
 
 //pokemon set up
 const pokeContainer = document.querySelector('#cards')
@@ -27,48 +19,57 @@ const pokeContainer = document.querySelector('#cards')
 let count = 1
 pokemonList.forEach(element => {
     //create name for calling the picture files
-    let numPrefix = count < 10 ? '00' : '0'
-    let imgName = (`${numPrefix}${count++}${element.ename}.png`)
-    //card container
-    let cardContainer = document.createElement('div')
-    cardContainer.className = "cardContainer"
-    //div for the full card
-    let frontCard = document.createElement('div')
-    frontCard.className = "front"
-    //figure
-    let fig = document.createElement('figure')
-    //figcaption
-    let cap = document.createElement('figcaption')
-    //image
-    let img = document.createElement('img')
-    img.src = `pokemonPics/${imgName}`
-    //set pokemon name
-    cap.textContent = element.ename
+    let cardContainer = makeCardFront(element);
 
-    //print on screen?
-
-    pokeContainer.appendChild(cardContainer)
-    cardContainer.appendChild(frontCard)
-    frontCard.appendChild(fig)
-
-    fig.appendChild(img)
-    fig.appendChild(cap)
+    //back of card
+    //makeCardBack(cardContainer);
   
 })
-//back of card-------------------
-pokemonList.forEach(element => {
-    
-    let backOfCard = document.createElement('div')
-    backOfCard.className = "back"
-    let backImg = document.createElement('img')
-    backImg.src = `assets\pokemonStuff\back.png`
-    backImg.className = "backImg"
-    backOfCard.appendChild(cardContainer)
-    backImg.appendChild(backOfCard)
-    
+
+function makeCardFront(element) {
+    let numPrefix = count < 10 ? '00' : '0';
+    let imgName = (`${numPrefix}${count++}${element.ename}.png`);
+    //card container
+    let cardContainer = document.createElement('div');
+    cardContainer.className = "cardContainer cardContainer--card";
+    //div for the full card
+    let frontCard = document.createElement('div');
+    frontCard.className = "front";
+    //figure
+    let fig = document.createElement('figure');
+    //figcaption
+    let cap = document.createElement('figcaption');
+    //image
+    let img = document.createElement('img');
+    img.src = `pokemonPics/${imgName}`;
+    //set pokemon name
+    cap.textContent = element.ename;
+    //print on screen?
+    pokeContainer.appendChild(cardContainer);
+    cardContainer.appendChild(frontCard);
+    frontCard.appendChild(fig);
+    fig.appendChild(img);
+    fig.appendChild(cap);
+    return cardContainer;
+}
+
+makeCardBack(cardContainer => {
+    let backOfCard = document.createElement('div');
+    backOfCard.className = "back";
+    let backImg = document.createElement('img');
+    backImg.className = "backImg";
+    backImg.src = '../assets/pokemonStuff/back.png';
+    backOfCard.appendChild(cardContainer);
+    backImg.appendChild(backOfCard);
 })
-
-
+//card flip----------------
+//set up
+// let card = document.getElementsByClassName('cardContainer')
+// //click to flip
+// card.addEventListener( 'click', () => {
+//     // console.log("click")
+//     card.classList.toggle('is-flipped');
+// });
 
 
 // for (i = 0; i < 5; i++) {
