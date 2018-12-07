758910ef245c812b3d2c9b6b959d0cce0940d1a9
Ben Shurts <benjaminshurts@gmail.com>
2018-12-06 21:28:25 -0700

    readme

--- README.md
+++ README.md
@@ -1,51 +1,55 @@
 # JSPortfolio
 
 #Deployable by simple Git push from your local Git repository to your Github repo to your public URL
-
+https://benshurts.com/
 #Has a good UI that utilizes proper, basic HTML, CSS and Flexbox
+flex box is on both home and javascript page.
 
 #Provides examples of Basic JavaScript code including:
+#Proper use of variables with proper scope
+Global and local variables are used in the pokemoncard generator
 
-Proper use of variables with proper scope
+#Good use of conditional logic and value comparison
 
-Good use of conditional logic and value comparison
 
-Proper use of String manipulation
+#Proper use of String manipulation
+used to get names of images
 
-Good use of Arrays
+#Good use of Arrays
+using
 
-Use of custom JavaScript objects
+#Use of custom JavaScript objects
 
-Demonstrates use of ES6 including:
+#Demonstrates use of ES6 including:
 
-Proper use of let and const variables
+#Proper use of let and const variables
 
-Use of Arrow functions
+#Use of Arrow functions
 
-Use of Strings using Template Literals
+#Use of Strings using Template Literals
 
-Proper declarations of Objects
+#Proper declarations of Objects
 
-Use of Import and Export statements
+#Use of Import and Export statements
 
-Demonstrates use of Basic Data Structures including:
+#Demonstrates use of Basic Data Structures including:
 
-Using Arrays to store and manipulate collections of data
+#Using Arrays to store and manipulate collections of data
 
-Use of Objects with key-value pairs
+#Use of Objects with key-value pairs
 
-Iteration through an Array using loops and Array methods
+#Iteration through an Array using loops and Array methods
 
-Demonstrates us of Object Oriented Programming techniques including:
+#Demonstrates us of Object Oriented Programming techniques including:
 
-Objects with properties and methods accessed using dot notation
+#Objects with properties and methods accessed using dot notation
 
-Objects using Constructors properly
+#Objects using Constructors properly
 
-Website is visually pleasing and easy to navigate and serves as a good example of learner's capability
+#Website is visually pleasing and easy to navigate and serves as a good example of learner's capability
 
-Website runs without errors
+#Website runs without errors
 
-Source code is well documented to describe where the features listed above are on display
+#Source code is well documented to describe where the features listed above are on display
 
-Website is clearly your own work and creatively demonstrates your web development skills
+#Website is clearly your own work and creatively demonstrates your web development skills
--- pokemonapi.js
+++ pokemonapi.js
@@ -1,4 +1,3 @@
-import { pokemonList } from './assets/pokemonStuff/pokemon.js'
 import { pokeItems } from "./assets/pokemonStuff/items.js"
 import { pokeTypes } from "./assets/pokemonStuff/types.js"
 import { pokedex } from "./assets/pokemonStuff/pokedex.js"
@@ -23,9 +22,8 @@ function pokeCard(name, hp, attack, defence, speed, id) {
         this.id = id
 }
 //-----------------filter types of pokemon--------------
-const GrassTypeFilter = pokeTypes["cname"]
-    // console.log(pokeTypes)
-console.log(GrassTypeFilter)
+// console.log(pokeTypes)
+// console.log(GrassTypeFilter)
 
 let normalType = pokedex.filter(NormalPokemon => NormalPokemon["type"] == "\u4e00\u822c")
 let fightType = pokedex.filter(FightingPokemon => FightingPokemon["type"] == "\u683c\u6597")
@@ -78,7 +76,7 @@ let typeArray = ["normal", "fight", "fly", "poison", "ground",
 //     })
 // }
 
-console.log(`${typeArray[0]}Btn`)
+// console.log(`${typeArray[0]}Btn`)
 
 //random all Button
 let randBtn = document.getElementById("randomize").addEventListener('click', () => {
@@ -175,123 +173,6 @@ let fairyBtn = document.getElementById("fairyBtn").addEventListener('click', () 
     MakeCards(fairyType)
 })
 
-function OneCard(CardObj) {
-    while (testSection.firstChild) {
-        testSection.removeChild(testSection.firstChild);
-    }
-    let randCard = CardObj[Math.floor(Math.random() * CardObj.length)]
-    let newRandCard = new pokeCard(randCard.ename, randCard.base.HP, randCard.base.Attack, randCard.base.Defense, randCard.base.Speed, randCard.id)
-
-    cardCollection.push(newRandCard)
-        // console.log(cardCollection)
-        //--- make cards ----
-    let card = document.createElement('div')
-    card.className = "card"
-    card.addEventListener('click', () => { card.classList.toggle('flip') })
-    testSection.appendChild(card)
-    let front = document.createElement('div')
-    front.className = "front"
-    card.appendChild(front)
-    let back = document.createElement('div')
-    back.className = "back"
-    card.appendChild(back)
-        //-----------inside of front of cards----------------
-    let imgName = (`${newRandCard.id}${newRandCard.name}.png`)
-        //name
-    let pokeName = document.createElement('h1')
-    pokeName.textContent = newRandCard.name
-    front.appendChild(pokeName)
-        //image
-    let frontImg = document.createElement('img')
-    frontImg.src = `pokemonPics/${imgName}`
-    front.appendChild(frontImg)
-        //stats
-    let statBox = document.createElement('div')
-    statBox.className = "statBox"
-    front.appendChild(statBox)
-        // ---------HP-----------
-    let hpParent = document.createElement('div')
-    hpParent.className = "statParent"
-    statBox.appendChild(hpParent)
-
-    let hpBar = document.createElement('div')
-    hpBar.className = "statBar"
-    hpParent.appendChild(hpBar)
-    hpBar.style.width = newRandCard.hp + "%"
-
-    let hp = document.createElement('p')
-    hp.textContent = "HP " + newRandCard.hp
-    hpBar.appendChild(hp)
-        //-----------attack------------
-    let atkParent = document.createElement('div')
-    atkParent.className = "statParent"
-    statBox.appendChild(atkParent)
-
-    let atkBar = document.createElement('div')
-    atkBar.className = "statBar"
-    atkParent.appendChild(atkBar)
-    atkBar.style.width = newRandCard.attack + "%"
-
-    let atk = document.createElement('p')
-    atk.textContent = "Atk " + newRandCard.attack
-    atkBar.appendChild(atk)
-        //-------------Def--------------
-    let defParent = document.createElement('div')
-    defParent.className = "statParent"
-    statBox.appendChild(defParent)
-
-    let defBar = document.createElement('div')
-    defBar.className = "statBar"
-    defParent.appendChild(defBar)
-    defBar.style.width = newRandCard.defence + "%"
-
-    let def = document.createElement('p')
-    def.textContent = "Def " + newRandCard.defence
-    defBar.appendChild(def)
-        //-------------speed-----------
-    let spdParent = document.createElement('div')
-    spdParent.className = "statParent"
-    statBox.appendChild(spdParent)
-
-    let spdBar = document.createElement('div')
-    spdBar.className = "statBar"
-    spdParent.appendChild(spdBar)
-    spdBar.style.width = newRandCard.speed + "%"
-
-    let spd = document.createElement('p')
-    spd.textContent = "Spd " + newRandCard.speed
-    spdBar.appendChild(spd)
-        //----------back of card content-----------------
-    let backImg = document.createElement('img')
-    backImg.src = './assets/pokemonStuff/back.png'
-    back.appendChild(backImg)
-    let copyBtn = document.createElement('button')
-    copyBtn.textContent = "Add to Deck"
-    back.appendChild(copyBtn)
-    copyBtn.addEventListener('click', (event) => {
-
-        cardCount += 1
-        cardCountCont.textContent = "Cards " + cardCount
-        let cloneCard = event.target.parentNode.parentNode
-        let newCard = cloneCard.cloneNode(true)
-        newCard.classList.toggle('flip')
-        let getButton = newCard.querySelector('button')
-        getButton.textContent = "Remove from Deck"
-        getButton.addEventListener('click', (event) => {
-
-                cardCount -= 1
-                cardCountCont.textContent = "Cards " + cardCount
-                let getCardRemove = event.target.parentNode.parentNode
-                getCardRemove.parentNode.removeChild(getCardRemove)
-
-            })
-            // console.log(getButton)
-        newCard.addEventListener('click', () => { newCard.classList.toggle('flip') })
-        newDeck.appendChild(newCard)
-
-
-    })
-}
 
 
 function MakeCards(CardObj) {
